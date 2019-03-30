60fce5605631d1e9a66f4dc47431e867b267e945
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-30 15:16:32 +0900

    override style

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Button>Normal</Button>
-        <Button primary>Primary</Button>
+        <Button>Normal Button</Button>
+        <TomatoButton>Tomato Button</TomatoButton>
       </div>
     );
   }
 }
 
 const Button = styled.button`
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
-
+  color: palevioletred;
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
 `;
 
 
