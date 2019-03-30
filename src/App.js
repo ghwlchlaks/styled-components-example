7b0293e88dfbcd3cc066438374af8e8e3054b956
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-30 15:09:10 +0900

    step1 example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Wrapper>
+        <Title>
+          Hello World
+        </Title>
+      </Wrapper>
     );
   }
 }
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 export default App;
